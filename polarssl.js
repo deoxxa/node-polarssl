6751476c33ad40a3c89a1f99836c3cbcf4f5fc61
deoxxa <deoxxa@fknsrs.biz>
2014-04-19 20:42:02 +0900

    add stream interface to Hash

--- polarssl.js
+++ polarssl.js
@@ -1,4 +1,11 @@
-var polarssl = module.exports = require("./build/Release/polarssl");
+var _polarssl = require("./build/Release/polarssl");
+
+var polarssl = module.exports = {
+  Hash: require("./lib/hash"),
+  Keygen: _polarssl.Keygen,
+  KeyRSA: _polarssl.KeyRSA,
+  Random: _polarssl.Random,
+};
 
 polarssl.createHash = function createHash(name) {
   return new polarssl.Hash(name);
